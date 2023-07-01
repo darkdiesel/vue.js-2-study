b0881ab82cdf3f4992f0e8527629813837916a65
Igor Peshkov <igor.peshkov@gmail.com>
2023-07-01 15:37:09 +0300

    Try v-for. And bind class with logic

--- js/app.js
+++ js/app.js
@@ -6,6 +6,28 @@ var app = new Vue({
     message: 'You loaded this page on ' + new Date().toLocaleString(),
     link: 'http://peshkov.website',
     markup: '<a href="http://peshkov.website" target="_blank">Click me!</a>',
+    users: [
+        {
+            'name' : 'Igor Peshkov',
+            'birthday' : '27.07.1988',
+            'is_admin': true,
+        },
+        {
+            'name' : 'Evgeny Peshkov',
+            'birthday' : '30.11.1990',
+            'is_admin': false,
+        },
+        {
+            'name' : 'Elena Peshkova',
+            'birthday' : '17.01.1966',
+            'is_admin': false,
+        },
+        {
+            'name' : 'Maria Peshkova',
+            'birthday' : '20.04.2013',
+            'is_admin': false,
+        },
+    ]
   },
   methods : {
     greet: function() {
