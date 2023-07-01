2d8bb3b464a306cff70cbce51a0c86a902c4ed19
Igor Peshkov <igor.peshkov@gmail.com>
2023-07-01 09:22:57 +0300

    Add first method. Try filter json on global $data. Directive v-model

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    name: 'Igor Peshkov',
+    job: 'Web Developer',
+    message: 'You loaded this page on ' + new Date().toLocaleString()
+  },
+  methods : {
+    greet: function() {
+        return 'My name is ' + this.name
+    }
   }
 })
