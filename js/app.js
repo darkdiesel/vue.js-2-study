2d3c6f1e5cbd02566d181485f7b031280cd5dd1c
Igor Peshkov <igor.peshkov@gmail.com>
2023-07-01 09:41:53 +0300

    Try v-html and v-bind directives

--- js/app.js
+++ js/app.js
@@ -3,7 +3,9 @@ var app = new Vue({
   data: {
     name: 'Igor Peshkov',
     job: 'Web Developer',
-    message: 'You loaded this page on ' + new Date().toLocaleString()
+    message: 'You loaded this page on ' + new Date().toLocaleString(),
+    link: 'http://peshkov.website',
+    markup: '<a href="http://peshkov.website" target="_blank">Click me!</a>',
   },
   methods : {
     greet: function() {
